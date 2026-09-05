efc63083ecf93142795107d0a8f709c2ab066ea7
Sufyaan Ibn Mubarack <sufyaan.ibnm@gmail.com>
2026-09-05 15:25:05 +0530

    update base path in Vite configuration for mockup deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/shaffedits-service-pages-redesign/",
+  base: "/MockUp/",
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
     outDir: "dist",
